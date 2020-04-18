646fe39cba94ba5a9862b24f5a35310354e8d463
tempesttswinson <tempestt.swinson@gmail.com>
2020-04-18 16:13:28 -0400

    additions added to exercise1 of AJax and Api Lab

--- Exercise1/exercise1.js
+++ Exercise1/exercise1.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
+	var display = $('.display');
+
 	//Get All Posts
 	$('#allPosts').click(()=>{
 		$.get(' https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts ', (posts)=>{
 
 			posts.forEach(()=>{
-				var p = $('<p></p>');
-				p.text(JSON.stringify(posts));
-				$('body').append(p);
+				display.text(JSON.stringify(posts));
 			})
 		})
 	})
@@ -17,9 +17,7 @@ $(document).ready(function(){
 		$.get('https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', {id: 10}, (posts)=>{
 
 			posts.forEach(()=>{
-				var p = $('<p></p>');
-				p.text(JSON.stringify(posts));
-				$('body').append(p);
+				display.text(JSON.stringify(posts));
 			})
 		})
 	})
@@ -30,9 +28,7 @@ $(document).ready(function(){
 		$.get(' https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/comments', {id: 12}, (comments)=>{
 
 			comments.forEach(()=>{
-				var p = $('<p></p>');
-				p.text(JSON.stringify(comments));
-				$('body').append(p);
+				display.text(JSON.stringify(comments));
 			})
 		})
 	})
@@ -42,9 +38,7 @@ $(document).ready(function(){
 		$.get(' https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts', {userID: 2}, (posts)=>{
 
 			posts.forEach(()=>{
-				var p = $('<p></p>');
-				p.text(JSON.stringify(posts));
-				$('body').append(p);
+				display.text(JSON.stringify(posts));
 			})
 		})
 	})
@@ -59,25 +53,87 @@ $(document).ready(function(){
 
 			.done((post) =>{
 				console.log(post)
-				var p = $('<p></p>');
-				p.text("id: " + post.id);
-				$('body').append(p);
+				display.text("id: " + post.id);
 			})
 	})
 
-	//Replace the post with id of 12 and render the responseJSON
+	//Replace the post with id of 14 and render the responseJSON
 	$('#replacePost').click(()=>{
 		$.ajax({
 			method: 'PUT',
-			url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts?userID=12',			
+			url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/14',			
 			data: {title: "new", body:"new body"},
 			complete: function(response){
-				console.log(response.responseJSON);
+				var response = response.responseJSON;
+				console.log(response);
+				display.text(JSON.stringify(response));
 			}
 
 		})
 
 	})
+
+	//Update the title of post with id of 14 and render responseJSON
+	$('#updateTitle14').click(()=>{
+		$.ajax({
+			method: 'PATCH',
+			url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/14',	
+			data: {title: " New Title Here"},
+			complete: function(response){
+				var response = response.responseJSON;
+				console.log(response);
+				display.text(JSON.stringify(response));
+			}
+
+		})
+
+	})
+
+	// Delete the post with id of 14 and render a success message
+	$('#deletePost14').click(()=>{
+		$.ajax({
+			method: 'DELETE',
+			url: 'https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts/14',
+			complete: function(response){
+				var response = response.responseJSON;
+				console.log(response);
+				display.text(JSON.stringify(response) + "Success!");
+			}
+		})
+	})
+
+
+// Display a list of posts.
+	// When the user clicks on a post, display all the comments from that post
+	// Display a link back to all posts
+
+	$('#displayPosts').click(()=>{
+		$.get(' https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/posts ', (posts)=>{
+			var i = 0;
+			posts.forEach(()=>{
+				i ++;
+				var li = $('<li></li');
+				li.text(JSON.stringify(posts[i]));
+				$('.displayList').append(li);
+			
+
+				$(li).click(()=>{
+
+					$.get(' https://my-json-server.typicode.com/zachhall/WIN2020_AjaxPromises/comments', (comments)=>{
+
+						var j = 0;
+						comments.forEach(()=>{
+							j ++;
+							console.log(comments[j]);
+							display.text(JSON.stringify(comments[j]));
+						 })
+
+					})
+				})
+			})		
+		})
+	})
+
 
 
 
